79c536b1d8fef870da1ca92d33392470bd91f142
gangireddy <nallagondu@hotmail.com>
2018-11-28 14:30:30 +0530

    Create math_Calculation.sh

---
+++ math_Calculation.sh
@@ -0,0 +1,27 @@
+[root@docker rhel]# cat calculation.sh 
+#!/bin/bash
+
+logfile="$HOME/.fcalc-log"
+
+date >> $logfile
+while /bin/true; do 
+	if [ "$(expr $RANDOM % 100)" -lt "70" ]; then 
+		x=`expr $(expr $RANDOM % 4) + 6`	
+	else
+		x=`expr $RANDOM % 5`
+	fi
+	y=`expr $RANDOM % 10`
+	rep=`expr $y \* $x`
+	urep="-1"
+	
+	while [ "$urep" -ne "$rep" ]; do 
+		read -p "$y * $x = ?" urep
+		if [ "$urep" -ne "$rep" ]; then
+			echo "$y * $x = ? rep: WRONG" >> $logfile
+			beep
+		else
+			echo "$y * $x = ?: $rep: Correct" >> $logfile
+		fi
+	done	
+done
+		
